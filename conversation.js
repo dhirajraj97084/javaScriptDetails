1818a897bb5da749e40ea41a2264f178b4930314
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-09-13 08:36:20 +0530

    nice

---
+++ conversation.js
@@ -0,0 +1,16 @@
+const num=33;//number
+console.log(num,typeof num)
+const str=String(num);// converted number to String
+console.log(str,typeof str);
+
+const isAdmine=true;//boolean
+console.log(isAdmine,typeof isAdmine);
+const converNum=Number(isAdmine); //boolean to Number
+console.log(converNum,typeof isAdmine);
+
+const num1=23;
+const num2=22;
+console.log(num1<=num2);//comparision
+
+const name="dhiraj"
+console.log(name.length, name.toUpperCase(),name.toLowerCase());

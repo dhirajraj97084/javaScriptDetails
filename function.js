47cb2767695b04bd2ddf9e1257f53aa6337c94b2
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-09-17 17:54:53 +0530

    funObject

--- function.js
+++ function.js
@@ -1,17 +1,37 @@
 function add(a,b){
     return a+b;
 }
-console.log(add(2,6));
+//console.log(add(2,6));
 
 function add(a,b){   
     console.log(a+b); //2  
 }
-console.log("hello dhiraj bhai") //1
-add(2,6);
+// console.log("hello dhiraj bhai"); //1
+// add(2,6);
 
 function foo(){
    return "hello";
 }
-const result=foo();
-console.log(result);
+
+
+// const result=foo();
+// console.log(result);
+
+const getobject={
+    name:"dhiraj",
+    email:"dhiraj@gmail.com"
+}
+
+function getallObject(anyobject){
+     return  `my name is ${anyobject.name} and my email is ${anyobject.email}`;
+}
+console.log(getallObject(getobject));
+
+
+function number(...num){
+    return num;
+}
+console.log(number(200,400,500,500));
+
+
 
